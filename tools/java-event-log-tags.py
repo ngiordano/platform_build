2ac1d959f766393927d9a3c438f4874f7f6a24d0
Daniel <daxx4182@gmail.com>
2013-03-12 13:39:29 +0800

    Revert "Use of 'python2' instead of 'python'."
    This reverts commit a5657b7912636bf460d2844c1686c938ddf40378.

--- tools/java-event-log-tags.py
+++ tools/java-event-log-tags.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 #
 # Copyright (C) 2009 The Android Open Source Project
 #
--- tools/warn.py
+++ tools/warn.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # This file uses the following encoding: utf-8
 
 import sys
