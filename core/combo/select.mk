15d137481781eaa14f4a464582a5a9999f642604
Nelson Giordano <ngiordano75@gmail.com>
2013-04-13 08:29:05 -0400

    update build

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -55,7 +55,7 @@ ifneq ($(combo_target),HOST_)
 (combo_target)RELEASE_CFLAGS += -Werror=strict-aliasing
 endif
 endif
-$(combo_target)GLOBAL_LDFLAGS := -Wl,-O2
+$(combo_target)GLOBAL_LDFLAGS := -Wl,-O3
 $(combo_target)GLOBAL_ARFLAGS := crsP
 
 $(combo_target)EXECUTABLE_SUFFIX :=
